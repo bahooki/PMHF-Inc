e8328c4ab069634605ec525873e8dca37610c99f
Allan1212 <adhollingsworth91@gmail.com>
2021-12-03 08:58:54 -0800

    Add files via upload
    drag and drop copy of python code

---
+++ script1.py
@@ -0,0 +1,10 @@
+# A first Python script
+import sys		# Load a library module
+print(sys.platform)
+print(2 ** 100)		# Raise 2 to a power
+x = 'Spam!'
+print(x * 8)		# String repetition
+#pause 10.0
+#output('Press Enter to Exit, unless you have something else to say? ')
+nextinput = input()			# <== ADDED
+#print(nextinput)
