3443a6e6d257ce544d192625b794112c1a8a32b3
Mario Cristiani <mcristia@varian.com>
2021-12-04 09:55:08 -0800

    trying to make this work, but getting confused on merge

--- yahoo-api.py
+++ yahoo-api.py
@@ -20,20 +20,3 @@ p_obj = json.loads(json_string) # converts json string to python object
 api_data = json.dumps(p_obj, indent=2) # pretty print
 
 print(api_data)
-
-#print(yahoo_financials.get_market_cap())
-#print(yahoo_financials.get_daily_low())
-=======
-from yahoofinancials import YahooFinancials
-
-ticker = 'NQ=F'
-yahoo_financials = YahooFinancials(ticker)
-
-# balance_sheet_data_qt = yahoo_financials.get_financial_stmts('quarterly', 'balance')
-# income_statement_data_qt = yahoo_financials.get_financial_stmts('quarterly', 'income')
-# all_statement_data_qt =  yahoo_financials.get_financial_stmts('quarterly', ['income', 'cash', 'balance'])
-# apple_earnings_data = yahoo_financials.get_stock_earnings_data()
-# apple_net_income = yahoo_financials.get_net_income()
-historical_stock_prices = yahoo_financials.get_historical_price_data('2020-09-01', '2020-10-01', 'monthly')
-print(historical_stock_prices)
-print(get_market_cap())
